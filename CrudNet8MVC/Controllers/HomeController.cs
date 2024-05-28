c3d0b12f66c89bebcdef38bd62f8191670a647f9
Luis Elian Pineda Revelo <luis.pineda.revelo@udla.edu.ec>
2024-05-28 18:13:22 -0500

    Ya se logro terminar el primer proyecto

--- CrudNet8MVC/Controllers/HomeController.cs
+++ CrudNet8MVC/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace CrudNet8MVC.Controllers
     {
         //Esto llamando a la data  para hacer uso de esos modelos 
         private readonly ApplicationDbContext _contexto;
-        
+
 
         //Este es el constructor y ahora puedo usarlo en cualquier metodo
         public HomeController(ApplicationDbContext contexto)
@@ -21,6 +21,111 @@ namespace CrudNet8MVC.Controllers
         {
             //Pasar la base de datos a una lista
             return View(_contexto.Contacto.ToList());
+        }
+        // Metodo solo para mostrar el formulario
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                // Agregar la fecha y hora
+                contacto.FechaCreacion = DateTime.Now;
+                _contexto.Contacto.Add(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        // Recibe el id para corregir
+        public IActionResult Editar(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+            return View(contacto);
+        }
+
+        [HttpPost]
+        //El validate previene ataques
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Update(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        //Vamos al detalle editar
+        [HttpGet]
+        // Recibe el id para corregir
+        public IActionResult Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+            return View(contacto);
+        }
+
+        //Ahora vamos a borrar
+        [HttpGet]
+        // Recibe el id para corregir
+        public IActionResult Borrar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+            return View(contacto);
+        }
+
+        // AHora el metodo si para borrar
+        [HttpPost, ActionName("Borrar")]
+        //El validate previene ataques
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> BorrarContacto(int? id)
+        {
+            var contacto = await _contexto.Contacto.FindAsync(id);
+            if (contacto == null)
+            {
+                return View();
+            }
+            //Borradoo fisico de la base de datos
+            _contexto.Contacto.Remove(contacto);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
