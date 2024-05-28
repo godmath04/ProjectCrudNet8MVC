f967e0e5e15316aaa95fd942e659f0779d5c6ac2
ASUS Vivobook <ASUS Vivobook@ASUSPERSONAL>
2024-05-28 00:12:34 -0500

    Add project files.

---
+++ CrudNet8MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudNet8MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet8MVC.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) :base(options){ 
+
+        }
+
+        //A partir de aqui se agregan los modelos, Cada uno es una tabla en la BD
+        public DbSet<Contacto> Contacto { get; set; }
+    }
+}
---
+++ CrudNet8MVC/Models/Contacto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudNet8MVC.Models
+{
+    public class Contacto
+    {
+        //Get y set para indicar que se puede escribir y leer del atributo
+        //Si se agrega [Key] se indica una llave primaria, a menos que se use el Id
+        [Key]
+        public int Id { get; set; }
+        //Hace que sea obligatorio
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        public String Telefono { get; set; }
+
+        [Required(ErrorMessage = "El celular es obligatorio")]
+        public String Celular { get; set; }
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public String Email { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+
+    }
+}
